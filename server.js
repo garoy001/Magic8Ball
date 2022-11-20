7fe4c408ac0fc138b1dc1cbf6bd7da5ec7d51dfb
Gabriel Royce <geroyceus@gmail.com>
2022-11-20 10:53:18 -0500

    Magic 8 ball express app created.

--- server.js
+++ server.js
@@ -4,6 +4,28 @@ require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const magicRes = [
+	'It is certain',
+	'It is decidedly so',
+	'Without a doubt',
+	'Yes definitely',
+	'You may rely on it',
+	'As I see it yes',
+	'Most likely',
+	'Outlook good',
+	'Yes',
+	'Signs point to yes',
+	'Reply hazy try again',
+	'Ask again later',
+	'Better not tell you now',
+	'Cannot predict now',
+	'Concentrate and ask again',
+	"Don't count on it",
+	'My reply is no',
+	'My sources say no',
+	'Outlook not so good',
+	'Very doubtful',
+];
 
 app.get('/greeting/:name', (req, res) => {
 	res.send(`Hello ${req.params.name}`);
@@ -14,6 +36,21 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 	const tipPercentage = parseInt(req.params.tipPercentage);
 	const newTotal = total * (tipPercentage / 100) + total;
 	res.send(`${newTotal}`);
+});
+
+app.get('/magic/:question', (req, res) => {
+	const answer = Math.floor(Math.random() * 20);
+	const question = req.params.question;
+	res.send(`
+	<html>
+		<body>
+			<h2>Question: ${question}</h2>
+			</br>
+			</br>
+			<h1>Answer: ${magicRes[answer]}</h1>
+		</body>
+	</html>
+	`);
 });
 
 app.listen(port, () => {
